ac8ef7840f59092b3a412c5e0f8ae24a685ecb4d
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-06-26 10:16:17 +0100

    clean up

--- src/main/java/com/codershub/springjpahibernate/CourseJdbcCommandlineRunner.java
+++ src/main/java/com/codershub/springjpahibernate/CourseJdbcCommandlineRunner.java
@@ -14,20 +14,6 @@ private  CourseSpringDataJpaRepository repository;
     public void run(String... args) throws Exception {
         Course course = new Course(1,"Learn java","Anthony");
         Course cours = new Course(2,"Learn swift","Anthony micheal");
-        repository.save(course);
-        repository.save(cours);
-
-        System.out.println(repository.findByAuthor("Anthony"));
-
-
-
-//        repository.insert(new Course(1,"Learn java Now","Anthony"));
-//        repository.insert(new Course(2,"Learn Andro Now","Anthony"));
-//        repository.insert(new Course(3,"Learn flutter Now","Anthony"));
-//        repository.insert(new Course(4,"Learn Node Now","Anthony"));
-//        repository.deleteById(1);
-//        System.out.println(repository.findById(4));
-//        System.out.println(repository.findById(3));
 
     }
 }
