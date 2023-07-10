72f88109431a4a81445272493bf5e96e043045a1
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-07-10 21:16:59 +0100

    add getter and setters

--- src/main/java/com/codershub/springjpahibernate/Course.java
+++ src/main/java/com/codershub/springjpahibernate/Course.java
@@ -1,6 +1,34 @@
 package com.codershub.springjpahibernate;
 
 class Course{
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
     @Override
     public String toString() {
         return super.toString();
